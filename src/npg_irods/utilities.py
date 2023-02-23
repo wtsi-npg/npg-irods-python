f87228d16f7aabc36fabd8541f86ee85b49a6ac5
Keith James <kdj@sanger.ac.uk>
2023-02-23 16:58:23 +0000

    Add reporting of counts to check/repair scripts where missing
    Add report of counts to check-common-metadata and repair-common-metadata.
    Fix naming of counts in repair-replicas num_passed -> numRepaired.

--- src/npg_irods/utilities.py
+++ src/npg_irods/utilities.py
@@ -423,7 +423,7 @@ def repair_replicas(
 
 def check_common_metadata(
     reader, writer, num_threads=1, num_clients=1, print_pass=True, print_fail=False
-) -> bool:
+) -> (int, int, int):
     """Read iRODS data object paths from a file and check that each one has correct
     common metadata, printing the results to a writer.
 
@@ -439,11 +439,13 @@ def check_common_metadata(
         print_fail: Print the paths of objects failing the check. Defaults to False.
 
     Returns:
-        True if all checks are done.
+        A tuple of the number of paths checked, the number of paths found to be correct
+        and the number of errors (paths with incomplete metadata and/or paths that
+        failed to be checked because of an exception).
     """
     with client_pool(num_clients) as bp:
 
-        def fn(i: int, path: str):
+        def fn(i: int, path: str) -> bool:
             success = False
 
             p = path.strip()
@@ -451,6 +453,7 @@ def check_common_metadata(
                 obj = DataObject(p, pool=bp)
                 if has_common_metadata(obj):
                     log.info("Common metadata complete", item=i, path=obj)
+                    success = True
                     if print_pass:
                         _print(p, writer)
                 else:
@@ -469,8 +472,6 @@ def check_common_metadata(
                     if print_fail:
                         _print(p, writer)
 
-                success = True
-
             except RodsError as re:
                 log.error(re.message, item=i, code=re.code)
                 if print_fail:
@@ -483,9 +484,10 @@ def check_common_metadata(
             return success
 
         with ThreadPool(num_threads) as tp:
-            succeeded = tp.starmap(fn, enumerate(reader))
+            results = tp.starmap(fn, enumerate(reader))
+            num_succeeded = results.count(True)
 
-        return all(succeeded)
+        return len(results), num_succeeded, len(results) - num_succeeded
 
 
 def repair_common_metadata(
@@ -496,7 +498,7 @@ def repair_common_metadata(
     num_clients=1,
     print_repair=True,
     print_fail=False,
-) -> bool:
+) -> (int, int, int):
     """Read iRODS data object paths from a file and ensure that each one has correct
     common metadata by making any necessary repairs, printing the results to a writer.
 
@@ -521,17 +523,22 @@ def repair_common_metadata(
             failed. Defaults to False.
 
     Returns:
-        True if all repairs were done.
+        A tuple of the number of paths checked, the number of paths whose metadata
+        was repaired and the number of errors (paths with incorrect metadata that
+        could not be fixed and/or failed to be fixed because of an exception).
     """
     with client_pool(num_clients) as bp:
 
-        def fn(i: int, path: str) -> bool:
+        def fn(i: int, path: str) -> (bool, bool):
             success = False
+            repair = False
 
             p = path.strip()
             try:
                 obj = DataObject(p, pool=bp)
-                if not has_common_metadata(obj):
+                if has_common_metadata(obj):
+                    log.info("Common metadata complete", item=i, path=obj)
+                else:
                     log.info(
                         "Common metadata incomplete; repairing",
                         item=i,
@@ -544,10 +551,10 @@ def repair_common_metadata(
                         has_type=has_type_metadata(obj),
                     )
 
-                    if ensure_common_metadata(obj, creator=creator):
+                    if repair := ensure_common_metadata(obj, creator=creator):
                         if print_repair:
                             _print(p, writer)
-                    success = True
+                success = True
             except RodsError as re:
                 log.error(re.message, item=i, code=re.code)
                 if print_fail:
@@ -557,12 +564,13 @@ def repair_common_metadata(
                 if print_fail:
                     _print(p, writer)
 
-            return success
+            return success, repair
 
         with ThreadPool(num_threads) as tp:
-            succeeded = tp.starmap(fn, enumerate(reader))
+            results, repaired = zip(*tp.starmap(fn, enumerate(reader)))
+            num_succeeded = results.count(True)
 
-        return all(succeeded)
+        return len(results), repaired.count(True), len(results) - num_succeeded
 
 
 def copy(src, dest, acl=False, avu=False, exist_ok=False, recurse=False) -> (int, int):
