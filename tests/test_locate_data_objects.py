735d6a1ebfdbbb7b9750051ee14890f762dbac01
Keith James <kdj@sanger.ac.uk>
2023-11-23 10:18:06 +0000

    Add reporting of Illumina QC JSON data objects
    Refactor and add a test for the Illumina data objects location CLI.
    
    Add find_qc_collection for Illumina.
    Add CollectionNotFound exception.
    
    Fix missing metadata in Illumina test fixtures.
    Fix file name underscores.

---
+++ tests/test_locate_data_objects.py
@@ -0,0 +1,35 @@
+from pytest import mark as m
+
+from conftest import BEGIN, LATEST
+from npg_irods.cli.locate_data_objects import illumina_updates
+
+
+@m.describe("Locating data objects in iRODS")
+class TestLocateDataObjects:
+    @m.context("When the MLWH contains records of updated Illumina metadata")
+    @m.context("When iRODS contains the corresponding data objects")
+    @m.it("Should print the paths of the data objects to be updated")
+    def test_illumina_updates(
+        self, capsys, illumina_synthetic_irods, illumina_synthetic_mlwh
+    ):
+        path = illumina_synthetic_irods / "12345"
+        expected = [
+            (path / p).as_posix()
+            for p in [
+                "12345#0.cram",
+                "12345#1.cram",
+                "12345#1_human.cram",
+                "12345#1_phix.cram",
+                "12345#2.cram",
+                "12345#888.cram",
+                "12345.cram",
+                "qc/12345#1.genotype.json",
+            ]
+        ]
+
+        np, ne = illumina_updates(illumina_synthetic_mlwh, BEGIN, LATEST)
+        stdout_lines = [line for line in capsys.readouterr().out.split("\n") if line]
+
+        assert np == 7, "Number of MLWH records processed"
+        assert ne == 0, "Number of errors"
+        assert stdout_lines == expected
