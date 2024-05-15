52c1d21c7eb005ad06888e9746ce4fd95c544766
Keith James <kdj@sanger.ac.uk>
2024-05-15 17:28:15 +0100

    Add ONT year runs meta-report
    Removed use of utcfromtimestamp, deprecated in Python 3.12

---
+++ src/npg_irods/cli/write_html_report.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2024 Genome Research Ltd. All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# @author Keith James <kdj@sanger.ac.uk>
+
+import argparse
+import sys
+
+import structlog
+from partisan.exception import RodsError
+from yattag import indent
+
+from npg_irods.cli.util import add_logging_arguments, configure_logging
+from npg_irods.html_reports import ont_runs_html_report_this_year
+from npg_irods.version import version
+
+description = """Writes an HTML report summarising data in iRODS.
+
+The reports include HTTP links to data objects and collections in iRODS. The links
+are only accessible if the report is rendered by a web server that can access the
+relevant iRODS zone. 
+
+Available reports are:
+
+    - ont: Oxford Nanopore sequencing data objects and collections.
+    
+    A summary of ONT runs for the calendar year to date.
+
+"""
+
+parser = argparse.ArgumentParser(
+    description=description,
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+)
+add_logging_arguments(parser)
+parser.add_argument(
+    "-o",
+    "--output",
+    help="Output filename.",
+    type=argparse.FileType("w", encoding="UTF-8"),
+    default=sys.stdout,
+)
+parser.add_argument(
+    "report",
+    help="Report type.",
+    type=str,
+    choices=["ont"],
+    nargs=1,
+)
+parser.add_argument(
+    "--zone",
+    help="Specify a federated iRODS zone in which to find data objects and/or "
+    "collections. This is not required if the target paths are on the local zone.",
+    type=str,
+)
+parser.add_argument(
+    "--version", help="Print the version and exit.", action="store_true"
+)
+
+args = parser.parse_args()
+configure_logging(
+    config_file=args.log_config,
+    debug=args.debug,
+    verbose=args.verbose,
+    colour=args.colour,
+    json=args.json,
+)
+log = structlog.get_logger("main")
+
+
+def main():
+    if args.version:
+        print(version())
+        sys.exit(0)
+
+    report = args.report[0]
+
+    try:
+        match report:
+            case "ont":
+                doc = ont_runs_html_report_this_year(zone=args.zone)
+            case _:
+                raise ValueError(f"Invalid HTML report type '{report}'")
+
+        print(indent(doc.getvalue()), file=args.output)
+    except RodsError as re:
+        log.error(re.message, code=re.code)
+        sys.exit(1)
+    except Exception as e:
+        log.error(e)
+        sys.exit(1)
---
+++ tests/ont/test_html_reports.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2024 Genome Research Ltd. All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# @author Keith James <kdj@sanger.ac.uk>
+
+from pytest import mark as m
+from yattag import indent
+
+from npg_irods.html_reports import ont_runs_html_report_this_year
+
+
+@m.describe("HTML Meta-Reports")
+class TestHTMLReports:
+    @m.context("When an ONT metadata report is generated")
+    @m.it("Contains the expected number of links to iRODS objects and collections")
+    def test_ont_runs_html_report(self, ont_synthetic_irods):
+        doc = ont_runs_html_report_this_year(zone="testZone")
+
+        # Uncomment to write the HTML to a file for manual inspection
+        #
+        # with open("ont_meta_report.html", "w") as f:
+        #     f.write(indent((doc.getvalue())))
+
+        links = [x for x in doc.result if x.startswith('<a href="/testZone/')]
+
+        expected_colls = 40
+        expected_objs = 3
+
+        assert len(links) == expected_colls + expected_objs
